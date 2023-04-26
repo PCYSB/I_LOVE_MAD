52166afbedf123badba3c4c238d40c45634996da
PCYSB <pcysb365@gmail.com>
2023-04-27 01:14:29 +0900

    Sigin up view2

--- app/src/main/java/com/example/i_love_mad_project/loginActivity.java
+++ app/src/main/java/com/example/i_love_mad_project/loginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.scwang.wave.MultiWaveHeader;
@@ -13,6 +14,9 @@ public class loginActivity extends AppCompatActivity {
     MultiWaveHeader wave_header;
     /////
     TextView tv_sign_up;
+    TextView tv_find_id;
+    TextView tv_find_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,9 @@ public class loginActivity extends AppCompatActivity {
         wave_header.setWaveHeight(40);
 
         tv_sign_up=findViewById(R.id.tv_sign_up);
+        tv_find_id=findViewById(R.id.tv_find_id);
+        tv_find_password=findViewById(R.id.tv_find_password);
+
         tv_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,5 +41,23 @@ public class loginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tv_find_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(loginActivity.this, FindID.class);
+                startActivity(intent);
+            }
+        });
+
+        tv_find_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(loginActivity.this, FindPassword.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
